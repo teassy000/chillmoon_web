c744ef106b8785b7f1ef77e276746c5888b544ca
teassy000 <teassy000@users.noreply.github.com>
2025-05-23 13:05:14 +0000

    deploy: 3efeab68b004ccf0719a3965076390d0ca8f48dc

---
+++ js/responsiveImageShortCode.js
@@ -0,0 +1,45 @@
+const Image = require("@11ty/eleventy-img");
+const path = require("path");
+
+async function responsiveImage(src, alt) {
+
+    const projectRoot = process.cwd();
+    const imageSrc =path.join(projectRoot, 'src', src) 
+    console.log("源图片路径为：" + imageSrc);
+    
+    const outputPath = process.env.OUTPUT_PATH || "_site";
+    const basePath = process.env.BASE_PATH || "/";
+    const metadata = await Image(imageSrc, {
+      widths: [400, 800, 1200],
+      formats: ["webp", "jpeg"],
+      outputDir: outputPath + "/img/",
+      urlPath: "/" + basePath + "/img/",
+      src: imageSrc,
+
+      filenameFormat: (hash, _src, width, format) => {
+        const ext = path.extname(_src);
+        const name = path.basename(_src, ext);
+        console.log("格式化后: " + `${name}-${width}w.${format}`);
+        return `${name}-${width}w.${format}`;
+      }
+    });
+
+
+
+    const originalUrl = metadata.jpeg[metadata.jpeg.length - 1].url;
+    const fixedSrc = originalUrl.replace(/\\/g, "/");
+    console.log("生成图片链接为：" + fixedSrc);
+
+    return `<div class="gallery-item">
+      ${Image.generateHTML(metadata, {
+        alt,
+        sizes: "(min-width: 768px) 50vw, 100vw",
+        loading: "lazy",
+        decoding: "async",
+        class: "thumbnail",
+        "data-original": fixedSrc,
+      })}
+    </div>`;
+  };
+
+  module.exports = responsiveImage;
---
+++ js/toggle-menu.js
@@ -0,0 +1,87 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelector('.nav-links');
+    
+    // 初始化状态
+    const init = () => {
+        // 检查本地存储状态
+        const savedState = localStorage.getItem('navExpanded');
+        if (savedState) {
+            navLinks.classList.toggle('active', savedState === 'false');
+            navToggle.setAttribute('aria-expanded', savedState);
+        }
+    };
+
+    function toggleNav() {
+        const isActive = navLinks.classList.toggle('active');
+        const blurOverlay = document.getElementById('blur-overlay');
+        navToggle.setAttribute('aria-expanded', isActive);
+        
+        // 同步模糊层状态
+        blurOverlay.style.opacity = isActive ? '1' : '0';
+        blurOverlay.style.pointerEvents = isActive ? 'auto' : 'none';
+
+        // 禁用滚动
+        document.body.style.overflow = isActive ? 'hidden' : '';
+
+    }
+
+    // 点击按钮切换
+    navToggle.addEventListener('click', (e) => {
+        e.preventDefault(); // 阻止事件冒泡
+        toggleNav();
+    });
+
+    // 点击模糊层关闭导航
+    const blurOverlay = document.getElementById('blur-overlay');
+        blurOverlay.addEventListener('click', () => {
+            if (navLinks.classList.contains('active')) {
+                toggleNav();
+            }
+    });
+
+    // 点击链接切换
+    navToggle.addEventListener('click', (e) => {
+        // 检查点击目标是否为 <a> 标签或其子元素
+        const clickedLink = e.target.closest('a');
+        if (clickedLink) {
+            toggleNav(); // 关闭导航
+            // 不需要阻止默认行为，链接会正常跳转
+        }
+    });
+
+    // 点击外部区域关闭
+    document.addEventListener('click', (e) => {
+        // 检查点击目标是否在导航内
+        const isNavLink = navLinks.contains(e.target);
+        const isNavToggle = navToggle.contains(e.target);
+        if (!isNavLink && !isNavToggle) {
+            navLinks.classList.remove('active');
+            navToggle.setAttribute('aria-expanded', 'false');
+        }
+    });
+
+    // 监听窗口大小变化
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 1000) {
+            navLinks.classList.remove('active');
+            navToggle.setAttribute('aria-expanded', 'false');
+
+            const blurOverlay = document.getElementById('blur-overlay');
+
+            // 同步模糊层状态
+            blurOverlay.style.opacity = '0';
+            blurOverlay.style.pointerEvents = 'none';
+        }
+    });
+
+    // 初始化
+    init();
+});
+
+const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+
+if (isMobile) {
+    document.documentElement.classList.add('mobile');
+}
